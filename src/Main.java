ad58814b523e0455e3a86c1141367a85444667c0
noamb <noam.braun@gmail.com>
2024-11-12 12:45:33 +0200

    trial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("please enter a random number:");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println(number);
+    }
+}
