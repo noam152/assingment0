1bfc5168b0eddfad8b9ac60798e0c9583781ac15
noamb <noam.braun@gmail.com>
2024-11-15 12:56:34 +0200

    trial

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,40 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("please enter a random number:");
+        System.out.print("please enter a even number:");
+        //long time = System.currentTimeMillis();
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println(number);
+        int a = 0;
+        System.out.println(isPrime(number));
+        int [] array = new int [number];
+            for (int i = 3; i < number; i = i + 2) {
+                if ( isPrime(i)) {
+                    array[i-3] = i;
+                    if(isPrime(number-array[i-3])) {
+                        a = number - array[i - 3];
+                    }
+                }
+                if(number == a + array[i - 3]) {
+                    System.out.println(number + "=" + a + "+" + array[i - 3]);
+                    break;
+                }
+            }
+
+        //long second = System.currentTimeMillis() - time0 / 1000;
+    }
+
+    public static boolean isPrime(int number) {
+        boolean isPrime = false;
+        if (number <= 1)
+            return false;
+        if (number == 2)
+            return true;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
