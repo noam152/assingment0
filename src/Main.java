77f489d5dc0d54b3e22b7ba0b1655f147e8fcc5e
noamb <noam.braun@gmail.com>
2024-11-18 21:43:52 +0200

    ready

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("please enter a even number:");
+        /** this program receive number from the user and return few checks on this number. at the first step, the program return two prime numbers that their connection equal to the
+         * user's number. at the second step, the program return the 2 smallest numbers that their subtraction bring the user's number. in the third step, the program count
+         * all the prime numbers between 2 to the user's number. in the forth step, the program divided the user's number into prime decomposition and print them.  the program
+         * compute the program's runtime and print this. in the end the program print my ID.
+         */
+        System.out.print("please enter a even number:");// ask for number from the user
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        long time = System.currentTimeMillis();
+        long time = System.currentTimeMillis();//start to count the time of the progress
         int a = 0;
         int a1 = 0;
         int b = 0;
         int c = 0;
         int d = 2;
-            for (int i = 3; i < number; i = i + 2) {
+            for (int i = 3; i < number; i = i + 2) {// loop that find 2 prime numbers that their connection equal to the user's number
                 if ( isPrime(i)) {
                     a1 = i;
                     if(isPrime(number - a1)) {
@@ -25,7 +30,7 @@ public class Main {
                     break;
                 }
             }
-        for (int i = 3; i < number; i = i + 2) {
+        for (int i = 3; i < number; i = i + 2) { //loop that bring the two smallest numbers that their subtraction equal to the user's number
             if (isPrime(i)) {
                 b = number + i;
                 if (isPrime(b) && number == b - i) {
@@ -35,7 +40,7 @@ public class Main {
             }
 
         }
-        for (int i = 3; i < number; i = i + 2) {
+        for (int i = 3; i < number; i = i + 2) { // calculate how many prime number there is between 2 to number
             if (isPrime(i)) {
                 c++;
             }
@@ -43,7 +48,7 @@ public class Main {
         System.out.println(c + " prime numbers in [" + 2 + "," + number + ")");
         System.out.print(number + "=" + 2);
         number = number / 2;
-        while (number != 1){
+        while (number != 1){ // loop that break the number into prime decomposition
             if (isPrime(d) && number % d == 0) {
                 System.out.print("*" + d);
                 number = number / d;
@@ -51,13 +56,12 @@ public class Main {
             else
                 d++;
         }
-        long second = (System.currentTimeMillis() - time )/ 1000;
         System.out.println();
-        System.out.println(second + " seconds");
+        System.out.println("Time: " + (System.currentTimeMillis() - time) / 1000.0 + " seconds"); //print the runtime of the program. the number represented in second so he divided by 1000.
         System.out.println("my ID is 211884077");
     }
 
-    public static boolean isPrime(int number) {
+    public static boolean isPrime(int number) { //outside action that check if number is prime or not
         boolean isPrime = false;
         if (number == 2)
             return true;
